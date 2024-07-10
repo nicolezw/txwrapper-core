8915a666b084d479010baa1514061e5c7c83b6e8
Nicole Wang <nicole.wang@circle.com>
2024-07-10 00:32:49 +0000

    testing decoding signingPayload

--- packages/txwrapper-core/src/core/decode/decodeSigningPayload.spec.ts
+++ packages/txwrapper-core/src/core/decode/decodeSigningPayload.spec.ts
@@ -4,7 +4,7 @@ import {
 	ASTAR_TEST_OPTIONS,
 	KUSAMA_TEST_OPTIONS,
 	TEST_BASE_TX_INFO,
-	TEST_METHOD_ARGS,
+	TEST_METHOD_ARGS, WESTEND_TEST_OPTIONS,
 } from '@substrate/txwrapper-dev';
 
 import {
@@ -110,5 +110,20 @@ describe('decodeSigningPayload', () => {
 		const decoded = decodeSigningPayload(signingPayload, ASTAR_TEST_OPTIONS);
 
 		expect(decoded.eraPeriod).toBe(0);
+	});
+	it('decode assets:setTeam', () => {
+		// https://docs.google.com/spreadsheets/d/1AEmPCwr8292otTzDo9AEQ64IvyDEpO0bhXq-DZC1H08/edit?gid=512370644#gid=512370644&range=49:49
+		const decoded = decodeSigningPayload("0x13cbe0fbb583b66c8514298b23b686bfc4777249f075c24b22a9ee134e258ec4", WESTEND_TEST_OPTIONS);
+
+		console.log("decoded: ")
+		console.log(decoded)
+	});
+
+	it('decode assets:transferOwnership msig init', () => {
+		// https://docs.google.com/spreadsheets/d/1AEmPCwr8292otTzDo9AEQ64IvyDEpO0bhXq-DZC1H08/edit?gid=512370644#gid=512370644&range=57:57
+		const decoded = decodeSigningPayload("0x290102000c3f9ca05d273c4ed62e2f5d0aa9300c78513875d0e1dfc03186731664a1ed824a68a6405cc230086c596c9e7cc90e79d7fb52292d7365cf6f1810579f97c07cbddc4b8673c5dee9fc96e7d2f68ec61b5bdfd045f523e3bbedc60af538700e95c100320fa6e9010000bcdb9b1895c5a03245a2aef24807f691a838f22be22f92e2b3a284afc33ae5000700e876481702093d00000400000008750f001000000067f9723393ef76214df0118c34bbbd3dbebc8ed46a10973a8c969d48fe7598c967f9723393ef76214df0118c34bbbd3dbebc8ed46a10973a8c969d48fe7598c900", WESTEND_TEST_OPTIONS);
+
+		console.log("decoded: ")
+		console.log(decoded)
 	});
 });
